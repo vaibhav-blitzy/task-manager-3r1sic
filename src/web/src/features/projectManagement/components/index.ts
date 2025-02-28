5ce3e33d5de53ce2a1945ddde31991bbac478342
Vaibhav Jaiswar <vaibhav@blitzy.com>
2025-02-28 21:38:42 +0530

    Add public-samples/task-manager-3r1sic/src/web/src/features/projectManagement/components/index.ts

---
+++ src/web/src/features/projectManagement/components/index.ts
@@ -0,0 +1,17 @@
+// Export all project management-related components
+export * from './ProjectList';
+export * from './ProjectCard';
+export * from './ProjectDetails';
+export * from './ProjectForm';
+export * from './ProjectMembers';
+export * from './ProjectCategory';
+export * from './ProjectKanban';
+export * from './ProjectHeader';
+export * from './ProjectTasks';
+export * from './ProjectProgress';
+export * from './ProjectSettings';
+export * from './ProjectAnalytics';
+export * from './ProjectFilters';
+export * from './AddProject';
+export * from './ProjectMenu';
+export * from './ProjectStatusBadge';
