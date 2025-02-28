00c6197baf07d07d052fcc5e0e864bb1f41aa897
Vaibhav Jaiswar <vaibhav@blitzy.com>
2025-02-28 21:38:52 +0530

    Add public-samples/task-manager-3r1sic/src/web/src/features/taskManagement/components/index.ts

---
+++ src/web/src/features/taskManagement/components/index.ts
@@ -0,0 +1,17 @@
+import TaskCard from '../../../components/molecules/TaskCard/TaskCard'; // Component for displaying individual task information in a card format
+import TaskList from '../../../components/organisms/TaskList/TaskList'; // Component for displaying a list of tasks
+import TaskBoard from '../../../components/organisms/TaskBoard/TaskBoard'; // Component for displaying tasks in a kanban board format
+import MobileTaskCard from '../../../components/mobile/MobileTaskCard/MobileTaskCard'; // Mobile-optimized component for displaying task information in a card format
+import CommentSection from '../../../components/organisms/CommentSection/CommentSection'; // Component for task comments
+import FileUploader from '../../../components/organisms/FileUploader/FileUploader'; // Component for uploading files to tasks
+import StatusBadge from '../../../components/molecules/StatusBadge/StatusBadge'; // Component for displaying task status
+
+export {
+  TaskCard, // Component for displaying individual task information in a card format
+  TaskList, // Component for displaying a list of tasks
+  TaskBoard, // Component for displaying tasks in a kanban board format
+  MobileTaskCard, // Mobile-optimized component for displaying task information in a card format
+  CommentSection, // Component for task comments
+  FileUploader, // Component for uploading files to tasks
+  StatusBadge // Component for displaying task status
+};
