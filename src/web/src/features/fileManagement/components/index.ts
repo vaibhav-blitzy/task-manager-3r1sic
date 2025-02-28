951ec886bae86836b24fc3236eab22a820bdf11d
Vaibhav Jaiswar <vaibhav@blitzy.com>
2025-02-28 21:38:32 +0530

    Add public-samples/task-manager-3r1sic/src/web/src/features/fileManagement/components/index.ts

---
+++ src/web/src/features/fileManagement/components/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Barrel file for file management components exports.
+ * Centralizes exports of all file-related components for easy importing throughout the application.
+ * 
+ * @version 1.0.0
+ */
+
+// Import file management components
+import FileAttachment from '../../../components/molecules/FileAttachment/FileAttachment';
+import FileUploader from '../../../components/organisms/FileUploader/FileUploader';
+
+// Re-export components for easy importing
+export { FileAttachment, FileUploader };
